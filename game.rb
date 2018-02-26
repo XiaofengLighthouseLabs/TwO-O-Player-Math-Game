648438d571e74b43a10b31141588a77fa00629c6
XiaofengLighthouseLabs <gaga0110@163.com>
2018-02-26 23:56:44 +0000

    fixed typo

--- game.rb
+++ game.rb
@@ -17,7 +17,7 @@ class Game
     if @player_one.lives > 0
       puts "player1 wins with a score of #{@player_one.lives}/3"
     elsif @player_two.lives > 0
-      puts "player wins with a score of #{@player_two.lives}/3"
+      puts "player2 wins with a score of #{@player_two.lives}/3"
     end
     puts "----Game Over----"
     puts "Good Bye!"
